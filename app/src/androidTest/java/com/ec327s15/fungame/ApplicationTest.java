540b866bd6be8d73351ab3e356644d2d255f6889
scortscort <scottthompson@wireless1x-155-41-99-131.bu.edu>
2015-04-17 15:32:55 -0400

    version 1
    first version of our game it is absolutely amazing

---
+++ app/src/androidTest/java/com/ec327s15/fungame/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.ec327s15.fungame;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
